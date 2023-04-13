230c1a369e83751ff2ebfb5a42cb1e9077e10ce1
samukavaz <soaresvaz@gmail.com>
2023-04-13 12:54:49 -0300

    corrigindo o app js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
 console.log("Tentando autenticar")
+console.log("Acrescentando página de contato")

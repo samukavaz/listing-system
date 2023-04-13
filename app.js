613c90e0b752bb9b59869441c15e5620a53c847c
samukavaz <soaresvaz@gmail.com>
2023-04-13 12:11:38 -0300

    voltando para o que era antes

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
 console.log("Tentando autenticar")
-
-console.log("Enviando para o GitHub")

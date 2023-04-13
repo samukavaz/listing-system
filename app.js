017091bda3fbca0e5aa36fbdd4fe6ed80fe9148c
samukavaz <soaresvaz@gmail.com>
2023-04-13 11:55:35 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
 console.log("Tentando autenticar")
+
+console.log("Enviando para o GitHub")
